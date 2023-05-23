9f1bf58faad909d107f6518aa4199ca7c9b6a571
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-23 22:15:47 +0900

    refactor: skeleton ui 스타일 분리

--- src/views/CartItemList/components/SkeletonCartItemList/SkeletonCartItemList.tsx
+++ src/views/CartItemList/components/SkeletonCartItemList/SkeletonCartItemList.tsx
@@ -1,72 +1,18 @@
-import { styled } from 'styled-components';
-import * as S from '../CartItemList/CartItemList.style';
-import {
-  PayingBox,
-  PayingContainer,
-} from '@views/Payment/components/ExpectedPayment/ExpectedPayment.style';
-import { CartItemContainer } from '@views/CartItem/components/CartItemBox/CartItemBox.style';
+import * as S from './SkeletonCartItemList.style';
 
 function SkeletonCart() {
   return (
     <>
-      <S.CartWrapper>
+      <S.SkeletonWrapper>
         {Array.from({ length: 3 }).map((_, index) => (
-          <SkeletonImage key={index} />
+          <S.SkeletonImage key={index} />
         ))}
-      </S.CartWrapper>
-      <PayingContainer>
-        <SkeletonPayingBox />
-      </PayingContainer>
+      </S.SkeletonWrapper>
+      <S.SkeletonPayingContainer>
+        <S.SkeletonPayingBox />
+      </S.SkeletonPayingContainer>
     </>
   );
 }
-
-const SkeletonImage = styled(CartItemContainer)`
-  background: linear-gradient(-90deg, #aaa, #f0f0f0, #aaa, #f0f0f0);
-  background-size: 400%;
-  animation: skeleton-animation 5s infinite ease-out;
-  border: none;
-
-  margin-bottom: 1rem;
-
-  padding-left: 50%;
-  padding-top: 30%;
-
-  @keyframes skeleton-animation {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-`;
-
-const SkeletonPayingBox = styled(PayingBox)`
-  background: linear-gradient(-90deg, #aaa, #f0f0f0, #aaa, #f0f0f0);
-  background-size: 400%;
-  animation: skeleton-animation 5s infinite ease-out;
-  border: none;
-
-  margin-bottom: 1rem;
-
-  padding-left: 50%;
-  padding-top: 50%;
-
-  @keyframes skeleton-animation {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-`;
 
 export default SkeletonCart;
--- src/views/ProductItemList/Components/SkeletonProduct/SkeletonProduct.tsx
+++ src/views/ProductItemList/Components/SkeletonProduct/SkeletonProduct.tsx
@@ -1,98 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Column, Row } from '@styles/style';
-import * as S from '@views/ProductItem/ProductItem.style';
-import { styled } from 'styled-components';
+import * as S from './SkeletonProduct.style';
 
 function SkeletonProduct() {
   return (
-    <Row>
+    <S.SkeletonRow>
       {Array.from({ length: 12 }).map((_, index) => (
-        <Column key={index}>
-          <S.ProductItemBox>
-            <StyleSkeletonImg />
+        <S.SkeletonColumn key={index}>
+          <S.SkeletonProductItem>
+            <S.StyleSkeletonImg />
             <div>
-              <SkeletonProductName></SkeletonProductName>
-              <SkeletonProductPrice></SkeletonProductPrice>
+              <S.SkeletonProductName></S.SkeletonProductName>
+              <S.SkeletonProductPrice></S.SkeletonProductPrice>
             </div>
-          </S.ProductItemBox>
-        </Column>
+          </S.SkeletonProductItem>
+        </S.SkeletonColumn>
       ))}
-    </Row>
+    </S.SkeletonRow>
   );
 }
-const SkeletonProductName = styled(S.ProductName)`
-  background: linear-gradient(-90deg, #aaa, #f0f0f0, #aaa, #f0f0f0);
-  background-size: 400%;
-  animation: skeleton-animation 5s infinite ease-out;
-  border: none;
-
-  @keyframes skeleton-animation {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-
-  width: 90%;
-  height: 1rem;
-
-  padding-left: 10%;
-  padding-top: 2rem;
-  border-radius: 3px;
-`;
-
-const SkeletonProductPrice = styled(S.ProductPrice)`
-  background: linear-gradient(-90deg, #aaa, #f0f0f0, #aaa, #f0f0f0);
-  background-size: 400%;
-  animation: skeleton-animation 5s infinite ease-out;
-  border: none;
-  width: 40%;
-  height: 1rem;
-
-  @keyframes skeleton-animation {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-
-  padding-left: 10%;
-  border-radius: 3px;
-  padding-top: 2rem;
-`;
-
-const StyleSkeletonImg = styled(S.ProductItemImage)`
-  background: linear-gradient(-90deg, #aaa, #f0f0f0, #aaa, #f0f0f0);
-  background-size: 400%;
-  animation: skeleton-animation 5s infinite ease-out;
-  border: none;
-
-  margin-bottom: 1rem;
-
-  padding-left: 50%;
-  padding-top: 50%;
-
-  @keyframes skeleton-animation {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-`;
 
 export default SkeletonProduct;
