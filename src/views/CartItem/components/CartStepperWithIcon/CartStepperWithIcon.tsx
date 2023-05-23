a6cfff3ee84d0af92af42fa5303161165774c122
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-23 22:38:32 +0900

    feat: 장바구니 페이지 이동시 장바구니 리스트 refresher 적용

--- src/views/CartItem/components/CartStepperWithIcon/CartStepperWithIcon.tsx
+++ src/views/CartItem/components/CartStepperWithIcon/CartStepperWithIcon.tsx
@@ -2,17 +2,22 @@ import { useNavigate } from 'react-router-dom';
 
 import * as S from './CartStepperWithIcon.style';
 import { useCartLength } from '@recoil/cart/withCartLength';
+import { useRefreshCartList } from '@recoil/cart/cartState';
 
 function CartStepperWithIcon() {
   const navigate = useNavigate();
   const cartListLength = useCartLength();
+  const refresher = useRefreshCartList();
 
   return (
     <S.CartWrapper
       type="button"
       aria-label="장바구니 페이지로 가기"
       role="button"
-      onClick={() => navigate('/cart')}
+      onClick={() => {
+        refresher();
+        navigate('/cart');
+      }}
     >
       <S.CartTitle>
         <S.CartText>장바구니</S.CartText>
